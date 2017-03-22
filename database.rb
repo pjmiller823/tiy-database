c65f3be9a50931907af45f1362ff6f47d1bc4aa9
Patrick Miller <pjmiller823@gmail.com>
2017-03-22 13:24:33 -0400

    pseudo code in comments with ideas for completion

---
+++ database.rb
@@ -0,0 +1,7 @@
+# Create a menu with add, search, and delete a person
+# - Welcome the user ans ask them if they would like to search, add, or delete a person.
+# Track name, phone number, address, position, salary, slack account, github account
+# - Class with attr reader and methods for all of the different variables.
+# -Create array that holds all pertinant information and is searchable.
+# reactive serach
+# - interracts with the attr reader and puts desired information.
