076b77365ff5b747944a386513d566de6fa518b5
Patrick Miller <pjmiller823@gmail.com>
2017-03-22 13:44:18 -0400

    pseudo code with ideas down and the menu completed

--- database.rb
+++ database.rb
@@ -1,7 +1,33 @@
-# Create a menu with add, search, and delete a person
-# - Welcome the user ans ask them if they would like to search, add, or delete a person.
+# ***Create a menu with add, search, and delete a person
+# ***- Welcome the user ans ask them if they would like to search, add, or delete a person.
+puts "Welcome to The Iron Yard employee database! Press 'A' to add a, 'S' to search for a person, and 'D' to delete someone. Have fun!"
+user_input = gets.chomp
+
+if user_input == "A"
+
+end
+
+if user_input == "S"
+
+end
+
+if user_input == "D"
+
+end
 # Track name, phone number, address, position, salary, slack account, github account
 # - Class with attr reader and methods for all of the different variables.
+class Database
+  def initialize(name, phone_number, address, position, salary, slack_account, github_account)
+    @name = name
+    @phone_number = phone_number
+    @address = address
+    @position = position
+    @salary = salary
+    @slack_account = slack_account
+    @github_account = github_account
+  end  
+end
+
 # -Create array that holds all pertinant information and is searchable.
 # reactive serach
 # - interracts with the attr reader and puts desired information.
